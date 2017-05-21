9ca8beb06490b3e4bb48326a06069d9647641f99
shahryarsaljoughi <s.shahryar75@gmail.com>
2017-05-22 01:53:12 +0430

    bug fixed

--- convex_hull/divide_and_conqure/sort_points.cpp
+++ convex_hull/divide_and_conqure/sort_points.cpp
@@ -9,7 +9,7 @@ struct point;
 //declarations:
 double polar_angle(point , point );
 double distance(point , point);
-void combine(vector<point>&, int , int , int );
+vector<point> combine(vector<point>&, vector<point>&, const point&);
 
 vector<point> sort(vector<point> points, int low, int high, const point &P0){
 	// unlike other sorts! result of sorting points is not
@@ -20,34 +20,37 @@ vector<point> sort(vector<point> points, int low, int high, const point &P0){
 		mid = (low+high)/2;
 		vector<point> a = sort(points, low, mid);
 		vector<point> b = sort(points, mid+1, high);
-		vector<point> c = combine(points, low, mid, high, P0);
+		vector<point> c = combine(a, b, P0);
+		return c;
+	}
+	else{
+		return points;
 	}
 }
 
-void combine(vector<point> points, int low, int mid, int high, const point &P0){
-	int i = low;
-	int j= mid+1;
-	int k=0;
+vector<point> combine(vector<point> &a, vector<point> &b, const point &P0){
+	int i = 0; // a[i]
+	int j= 0;  // b[j]
+	int k=0;   // result[k]
 	
 	vector<point> result;
-	//result.resize(high - low + 1);
 	
-	while(i<=mid && j<=high){
-		if ( polar_angle(P0, points.at(i)) < polar_angle(P0, points.at(j)) ){
+	while(i<a.size() && j<b.size()){
+		if ( polar_angle(P0, a.at(i)) < polar_angle(P0, b.at(j)) ){
 			result.resize(result.size()+1);
-			result.at(k) = points.at(i);
+			result.at(k) = a.at(i);
 			i++;k++;
 		}
-		else if ( polar_angle(P0, points.at(i)) == polar_angle(P0, points.at(j)) ){
-			if distance(P0, points.at.(i)) < distance(P0, points.at(j)){
+		else if ( polar_angle(P0, a.at(i)) == polar_angle(P0, b.at(j)) ){
+			if distance(P0, a.at.(i)) < distance(P0, b.at(j)){
 				result.resize(result.size()+1);
-				result.at(k) = points.at(j);
+				result.at(k) = b.at(j);
 				k++; j++;
 				i++; // points[i] is omitted ! (it is not a vertex!!!) ----> pay attention
 			}
 			else{
 				result.resize(result.size()+1);
-				result.at(k) = points.at(i);
+				result.at(k) = a.at(i);
 				k++; 
 				i++;
 				j++; // we know that points[j] is an inner point not a vertex! 
@@ -55,28 +58,24 @@ void combine(vector<point> points, int low, int mid, int high, const point &P0){
 		}
 		else{ // polar_angle(j) is smaller
 			result.resize(result.size()+1);
-			result.at(k) = points.at(j);
+			result.at(k) = b.at(j);
 			k++;
 			j++;
 		}
 	}
 	
-	while(i<=mid){
+	while(i<a.size()){
 		result.resize(result.size()+1);
 		result.at(k) = points.at(i);
 		i++;k++;
 	}
-	while(j<=high){
+	while(j<b.size()){
 		result.resize(result.size()+1);
 		result.at(k) = points.at(j);
 		j++;k++;
 	}
+	return result;
 }
-	
-			
-		
-
-
 
 
 double polar_angle(point p0, point p1){
